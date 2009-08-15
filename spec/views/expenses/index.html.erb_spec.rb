aac8f7ce92127396c2cb145376ce31dde57630a4
Petyo Ivanov <underlog@gmail.com>
2009-08-15 18:39:51 +0300

    Fixed spec view.

--- spec/views/expenses/index.html.erb_spec.rb
+++ spec/views/expenses/index.html.erb_spec.rb
@@ -2,11 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/expenses/index" do
   before(:each) do
+    assigns[:expense] = mock_model(Expense)
     render 'expenses/index'
   end
 
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/expenses/index])
-  end
+  it { should respond_with :success }
+
 end
