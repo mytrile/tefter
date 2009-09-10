9934c326b0cb6965affcdd011def97bd9fa7d17f
Petyo Ivanov <underlog@gmail.com>
2009-09-10 16:49:22 +0300

    Fixed stupid typo.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_piggyback_session',
+  :key         => '_piggybank_session',
   :secret      => '239f265612cb347cc8f4b177c58031e794c58de64cce3033255b1ca25fe9118a59d7e68c133d0ee7e90b8c84e3e6107aba128a6f400c9313162bf07c1185366f'
 }
 
