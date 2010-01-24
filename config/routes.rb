394f16e21ff9ef9395f14bb3493b7a3a67445f01
Petyo Ivanov <underlog@gmail.com>
2010-01-24 20:23:04 +0200

    Some copy, looks updated.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => "home"
   map.sign_up "sign_up", :controller => "users", :action => "new"
   map.sign_in "sign_in", :controller => "user_sessions", :action => "new"
+  map.sign_out "sign_out", :controller => "user_sessions", :action => "destroy"
   map.resource :users
 
   # rspec needs this
